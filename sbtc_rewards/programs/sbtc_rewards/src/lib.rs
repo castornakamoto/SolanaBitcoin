8b09da79b23367e6f512221f82e78df0f97d3d50
Alex <ulrickpspgo@gmail.com>
2023-11-22 21:34:39 +0100

    Added deposit (lock) method

--- sbtc_rewards/programs/sbtc_rewards/src/lib.rs
+++ sbtc_rewards/programs/sbtc_rewards/src/lib.rs
@@ -1,26 +1,51 @@
 use anchor_lang::prelude::*;
+use solana_program::program::invoke;
+use solana_program::system_instruction;
 
 declare_id!("9jEs9Sy93JMhY8VsVA7Z5TPA6Mi6eRWVBMWMcqppr1Ss");
 
 #[program]
 pub mod pdas {
+
     use super::*;
 
     pub fn create_ledger(
         ctx: Context<CreateLedger>,
     ) -> Result<()> {
         let ledger_account = &mut ctx.accounts.ledger_account;
-        ledger_account.balance = 0;
+        ledger_account.locked_amount = 0;
         ledger_account.timestamp = Clock::get()?.unix_timestamp;
         Ok(())
     }
 
-    pub fn modify_ledger(
-        ctx: Context<ModifyLedger>,
-        new_balance: u32,
+    pub fn deposit_funds(
+        ctx: Context<DepositFunds>,
+        amount: u64,
     ) -> Result<()> {
         let ledger_account = &mut ctx.accounts.ledger_account;
-        ledger_account.balance = new_balance;
+        let wallet = &ctx.accounts.wallet;
+        let system_program = &ctx.accounts.system_program;
+
+
+        // Transfer funds from the wallet to the ledger account
+        invoke(
+            &system_instruction::transfer(
+                wallet.to_account_info().key,
+                ledger_account.to_account_info().key,
+                amount,
+            ),
+            &[
+                wallet.to_account_info(),
+                ledger_account.to_account_info(),
+                system_program.to_account_info(),
+            ],
+        )?;
+
+        // Update the ledger balance
+        ledger_account.locked_amount = ledger_account.locked_amount.checked_add(amount).ok_or_else(|| {
+            anchor_lang::error!(ErrorCode::Overflow)
+        })?;
+
         Ok(())
     }
 }
@@ -30,7 +55,7 @@ pub struct CreateLedger<'info> {
     #[account(
     init,
     payer = wallet,
-    space = 104,
+    space = 8 + 128,
     seeds = [wallet.key().as_ref(), b"_"],
     bump
     )]
@@ -48,8 +73,27 @@ pub struct ModifyLedger<'info> {
     pub wallet: Signer<'info>,
 }
 
+#[derive(Accounts)]
+pub struct DepositFunds<'info> {
+    #[account(mut)]
+    pub ledger_account: Account<'info, Ledger>,
+    #[account(mut)]
+    pub wallet: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Overflow when adding to the balance")]
+    Overflow,
+    #[msg("Insufficient funds for the transaction")]
+    InsufficientFunds,
+    #[msg("Failed to retrieve balance")]
+    BalanceRetrievalFailure,
+}
+
 #[account]
 pub struct Ledger {
-    pub balance: u32,
+    pub locked_amount: u64,
     pub timestamp: i64, // Add the timestamp field
 }
--- sbtc_rewards/tests/pdas.ts
+++ sbtc_rewards/tests/pdas.ts
@@ -1,9 +1,16 @@
 import * as anchor from "@project-serum/anchor";
 import { Pdas } from "../target/types/pdas";
+import { BN } from "bn.js";
+import { generateAndSaveKeypair, loadKeypair } from "./keypair-utils"; // Adjust the path as needed
 
 
 
-function shortKey(key: anchor.web3.PublicKey) {
+/**
+ * Returns a shortened version of a public key, useful for displaying or logging.
+ * @param key - The public key to shorten.
+ * @returns The first 8 characters of the public key as a string.
+ */
+function shortKey(key: anchor.web3.PublicKey): string {
   return key.toString().substring(0, 8);
 }
 
@@ -14,18 +21,14 @@ describe("pdas", () => {
   anchor.setProvider(provider);
   const program = anchor.workspace.Pdas as anchor.Program<Pdas>;
 
-  async function generateKeypair() {
-    let keypair = anchor.web3.Keypair.generate();
-    await provider.connection.requestAirdrop(
-      keypair.publicKey,
-      2 * anchor.web3.LAMPORTS_PER_SOL
-    );
-    await new Promise( resolve => setTimeout(resolve, 3 * 1000) ); // Sleep 3s
-    return keypair;
-  }
-
-  async function derivePda(pubkey: anchor.web3.PublicKey) {
-    let [pda, _] = await anchor.web3.PublicKey.findProgramAddressSync(
+  
+  /**
+   * Derives a program-derived address (PDA) based on the given public key and program ID.
+   * @param pubkey - The public key used to derive the PDA.
+   * @returns The derived program address.
+   */
+  async function derivePda(pubkey: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> {
+    const [pda, _] = await anchor.web3.PublicKey.findProgramAddressSync(
       [
         pubkey.toBuffer(),
         Buffer.from("_")
@@ -35,10 +38,16 @@ describe("pdas", () => {
     return pda;
   }
 
+
+  /**
+   * Creates a ledger account by invoking the appropriate program method.
+   * @param pda - The program-derived address (PDA) associated with the ledger account.
+   * @param wallet - The keypair of the wallet initiating the account creation.
+   */
   async function createLedgerAccount(
-    pda: anchor.web3.PublicKey, 
+    pda: anchor.web3.PublicKey,
     wallet: anchor.web3.Keypair
-  ) {
+  ): Promise<void> {
     await program.methods.createLedger()
       .accounts({
         ledgerAccount: pda,
@@ -48,56 +57,85 @@ describe("pdas", () => {
       .rpc();
   }
 
-  async function modifyLedger(
-    newBalance: number,
-    wallet: anchor.web3.Keypair, 
-  ) {
 
+  /**
+   * Deposits a specified amount of tokens into a ledger account associated with the given wallet.
+   * @param amount - The amount of tokens to deposit.
+   * @param wallet - The keypair of the wallet initiating the deposit.
+   */
+  async function depositFunds(
+    amount: anchor.BN,
+    wallet: anchor.web3.Keypair
+  ): Promise<void> {
+    console.log("\n");
     console.log("--------------------------------------------------");
-    let data;
+
+    //Log current wallet public key
+    console.log(`Current wallet: ${shortKey(wallet.publicKey)}`);
+
+    // Derive the PDA for the wallet
     let pda = await derivePda(wallet.publicKey);
 
+    // Check if the ledger account associated with the PDA exists; otherwise, create it
     console.log(`Checking if account ${shortKey(pda)} exists`);
     try {
+      const data = await program.account.ledger.fetch(pda);
+      console.log("Ledger account exists.");
 
-      data = await program.account.ledger.fetch(pda);
-      console.log("It does.");
-    
+      // Display the current balance in the source wallet in SOL
+      const initialBalance = await provider.connection.getBalance(wallet.publicKey);
+      console.log(`Initial balance in source wallet: ${initialBalance / 1e9} SOL`);
+
+      // Invoke the depositFunds method of the program
+      console.log(`Depositing ${amount.toNumber() / 1e9} SOL tokens...`);
+      await program.methods.depositFunds(amount)
+        .accounts({
+          ledgerAccount: pda,
+          wallet: wallet.publicKey,
+        })
+        .signers([wallet])
+        .rpc();
+
+      // Fetch and display the updated ledger account data after the deposit
+      const updatedData = await program.account.ledger.fetch(pda);
+      console.log(`Timestamp: ${updatedData.timestamp}   Locked amount: ${updatedData.lockedAmount.toNumber()  / 1e9} SOL tokens`);
+
+      // Get the current balance of the wallet and display it in SOL
+      const endBalance = await provider.connection.getBalance(wallet.publicKey);
+      console.log(`Current balance in source wallet: ${endBalance / 1e9} SOL`);
     } catch (e) {
-    
-      console.log("It does NOT. Creating...");
+      console.log("Ledger account does NOT exist. Creating...");
+
+      // Create the ledger account if it doesn't exist
       await createLedgerAccount(pda, wallet);
-      data = await program.account.ledger.fetch(pda);
-    };
+      console.log("Ledger account created.");
 
-    console.log("Success.");
-    console.log("Data:")
-    console.log(`    Color: ${data.color}   Balance: ${data.balance}`);
-    console.log(`Modifying balance of ${data.color} from ${data.balance} to ${newBalance}`);
-
-    await program.methods.modifyLedger(newBalance)
-      .accounts({
-        ledgerAccount: pda,
-        wallet: wallet.publicKey,
-      })
-      .signers([wallet])
-      .rpc();
-
-    data = await program.account.ledger.fetch(pda);
-    console.log("New Data:")
-    console.log(`    Timestamp: ${data.timestamp}   Balance: ${data.balance}`);
-    console.log("Success.");
+      // Retry the deposit operation
+      await depositFunds(amount, wallet);
+    }
   }
 
+  it('An example of PDAs in action', async () => {
 
-  it("An example of PDAs in action", async () => {
+      // Load or generate keypairs for test accounts
+      const testKeypair1 = await loadKeypair('testKeypair1') || await generateAndSaveKeypair('testKeypair1');
+      const testKeypair2 = await loadKeypair('testKeypair2') || await generateAndSaveKeypair('testKeypair2');
 
-    const testKeypair1 = await generateKeypair();
-    await modifyLedger(2, testKeypair1);
-    await modifyLedger(4, testKeypair1);
+    /*  // Request SOL from the faucet
+      await provider.connection.requestAirdrop(
+        testKeypair1.publicKey,
+        2 * anchor.web3.LAMPORTS_PER_SOL
+      );
+      await new Promise( resolve => setTimeout(resolve, 3 * 1000) ); // Sleep 
 
-    const testKeypair2 = await generateKeypair();
-    await modifyLedger(3, testKeypair2);
-    await modifyLedger(3, testKeypair2);
+      await provider.connection.requestAirdrop(
+        testKeypair2.publicKey,
+        2 * anchor.web3.LAMPORTS_PER_SOL
+      );
+      await new Promise( resolve => setTimeout(resolve, 3 * 1000) ); // Sleep  */
+
+      // Test functions
+      await depositFunds(new BN(50000000), testKeypair1);
+      await depositFunds(new BN(10000000), testKeypair2);
   });
 });
